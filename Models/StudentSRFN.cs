f21a07abd436ce2b13d9b7d31e3ddae12027f225
Programming8 <Programming8@IT8>
2019-08-23 17:46:19 -0400

    1st upload aug23th2019 5h46pm

---
+++ Models/StudentSRFN.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CollegeManagementSystem.Models
+{
+    public class StudentSRFN
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //[Required(ErrorMessage = "Please tell us your birth date")]
+        //[Range(typeof(DateTime), "1/1/1900", "12/31/2010",ErrorMessage = "{0} must be between {1} and {2}")]
+        public DateTime BirthDate { get; set; }
+        public string EmailAddr { get; set; }
+        public string Country { get; set; }
+
+    }
+}
